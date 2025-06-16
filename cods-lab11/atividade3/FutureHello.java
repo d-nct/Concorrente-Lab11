79c5e45edcff5595201fc6180fac3cee22167f8f
Daniel Nocito <nocito@ufrj.br>
2025-06-16 18:58:12 -0300

    Implementa a lista de futuros circular

--- cods-lab11/atividade3/FutureHello.java
+++ cods-lab11/atividade3/FutureHello.java
@@ -12,11 +12,13 @@ import java.util.concurrent.Future;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 //classe do método main
 public class FutureHello  {
-  private static final long N = 2000000;
+  private static final long N = 1000000000;
   private static final int NTHREADS = 10;
+  private static final int LIST_CAPACITY = NTHREADS * 10;
 
   public static void main(String[] args) {
     //cria um pool de threads (NTHREADS)
@@ -24,59 +26,58 @@ public class FutureHello  {
     //cria uma lista para armazenar referencias de chamadas assincronas
     List<Future<Boolean>> list = new ArrayList<Future<Boolean>>();
 
-    System.out.print("Enviando " + N + " tarefas para o pool de threads... ");
-    for (long i = 1; i <= N; i++) {
-      Callable<Boolean> worker = new Primo(i, 0); // Instancia os objetos que executam o processamento
-      Future<Boolean> submit = executor.submit(worker); // Executa o processamento via pool de threads
-      list.add(submit); // Coloca o ticket para o resultado na lista
+    // Variáveis de controle para a lista de tamanho fixo
+    long tarefasSubmetidas = 0;
+    long primosEncontrados = 0;
+
+    System.out.println("Iniciando checagem de primos até " + N + " com " + NTHREADS + "threads via pull de threads...");
+
+    while (tarefasSubmetidas < N) {
+      // Submete as tarefas até encher a lista
+      while (list.size() < LIST_CAPACITY && tarefasSubmetidas < N) {
+        Callable<Boolean> worker = new Primo(tarefasSubmetidas++, 0); // Instancia os objetos que executam o processamento
+        Future<Boolean> submit = executor.submit(worker); // Executa o processamento via pool de threads
+        list.add(submit); // Coloca o ticket para o resultado na lista
+      }
+
+      // Processa a lista
+      Iterator<Future<Boolean>> iterator = list.iterator();
+      while (iterator.hasNext()) {
+        Future<Boolean> future = iterator.next();
+        if (future.isDone()) { // não bloqueante
+          try {
+            if (future.get()) { // dentro do bloco não bloqueante, a func bloqueante não bloqueia
+              primosEncontrados++;
+            }
+          } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+          }
+          iterator.remove(); // Pulo do gato: desaloca a tarefa da lista
+        }
+      }
     }
-    System.out.println("Sucesso!");
 
-    //recupera os resultados e faz o somatório final
-    long sum = 0;
-    long tarefasProcessadas = 0; // Para mostrar o progresso!
-    boolean imprimiu25 = false;
-    boolean imprimiu50 = false;
-    boolean imprimiu75 = false;
-    long umQuarto = N / 4;
-    long doisQuartos = N / 2;
-    long tresQuartos = N * 3 / 4;
-
-    System.out.print("Progresso até 25%... ");
+    // Processa as tarefas sobressalentes
+    System.out.println("Tarefas submetidas. Aguardando as " + list.size() + " últimas terminarem...");
     for (Future<Boolean> future : list) {
       try {
-        if (future.get()) sum++; //bloqueia se a computação nao tiver terminado
-        
-        // IMPLEMENTAR LÓGICA DE BARRA DE PROGRESSO: PRINT EM 25%, 50% e 75% de progresso 
-        tarefasProcessadas++;
-        if (!imprimiu25 && tarefasProcessadas >= umQuarto) {
-          System.out.println("Ok!");
-          System.out.print("Progresso até 50%... ");
-          imprimiu25 = true;
-        } 
-        if (!imprimiu50 && tarefasProcessadas >= doisQuartos) {
-          System.out.println("Ok!");
-          System.out.print("Progresso até 75%... ");
-          imprimiu50 = true;
-        } 
-        if (!imprimiu75 && tarefasProcessadas >= tresQuartos) {
-          System.out.println("Ok!");
-          System.out.print("Progresso até 100%... ");
-          imprimiu75 = true;
+        if (future.get()) { // bloqueante
+          primosEncontrados++;
         }
-      } catch (InterruptedException e) {
-        break;
-      } catch (ExecutionException e) {
+      } catch (InterruptedException | ExecutionException e) {
         e.printStackTrace();
       }
     }
-    System.out.println("Ok!");
+    System.out.println("Terminei!");
 
     // Interface com Usuário
     System.out.println("\nRESUMO");
-    System.out.println("------");
+    System.out.println("======");
     System.out.println("Verificados inteiros de 1 a " + N);
-    System.out.println("Total de primos encontrados: " + sum);
+    System.out.println("Número de Threads: " + NTHREADS);
+    System.out.println("Capacidade da lista de futuros: " + LIST_CAPACITY);
+    System.out.println("---");
+    System.out.println("Total de primos encontrados: " + primosEncontrados);
 
     executor.shutdown();
   }
